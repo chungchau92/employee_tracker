36c816c4603c9beea411c9f1309d37f5bd51e819
chungchau <chungchau92@gmail.com>
2022-12-12 17:31:06 +0700

    create inquirer question

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+
+const questions = {
+    name: "question",
+    type: "list",
+    message: "What would you like to do",
+    choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "View Employee by Manager",
+        "View Employee by Department",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee Manager",
+        "Update Employes Role",
+        "Delete Departments",
+        "Delete Role",
+        "Delete Employee",
+        "view bubget of Department",
+        "Quit"
+    ]
+}
