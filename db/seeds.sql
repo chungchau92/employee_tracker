f86d7c80a85a4d51613245b08f078171a03c6ff1
chungchau <chungchau92@gmail.com>
2022-12-14 17:42:15 +0700

    add function viewDepartments

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,7 +15,7 @@ INSERT INTO role (title, salary, department_id)
             ("Lawyer", 190000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES  ("John", "Doe", 1),
+    VALUES  ("John", "Doe", 1 ),
             ("Mike", "Chan", 2),
             ("Ashley", "Rodriguez", 3),
             ("Kevin", "Tupik", 4),
